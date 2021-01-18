54f52ab54fad2368725f6f7b7f8b1ca6a48b283c
Patrick Pacher <patrick.pacher@gmail.com>
2021-01-18 11:25:22 +0100

    Add support to encode a struct to file

---
+++ conf/encode_utils.go
@@ -0,0 +1,180 @@
+package conf
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// WriteSectionsTo writes all sections to w .
+func WriteSectionsTo(sections Sections, w io.Writer) error {
+	for _, sec := range sections {
+		if _, err := fmt.Fprintf(w, "[%s]\n", sec.Name); err != nil {
+			return err
+		}
+
+		for _, opt := range sec.Options {
+			if _, err := fmt.Fprintf(w, "%s= %s", opt.Name, opt.Value); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// ConvertToFile converts x to a File. x is expected to be or point to a struct
+// type.
+func ConvertToFile(x interface{}, path string) (*File, error) {
+	val := reflect.ValueOf(x)
+	f := &File{
+		Path: path,
+	}
+
+	if err := encodeFile(val, f); err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func encodeFile(val reflect.Value, result *File) error {
+	kind := getKind(val)
+
+	if kind == reflect.Ptr {
+		val = reflect.Indirect(val)
+		return encodeFile(val, result)
+	}
+
+	if kind != reflect.Struct {
+		return fmt.Errorf("cannot convert type %s to file", kind)
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldValue := val.Field(i)
+		fieldType := val.Type().Field(i)
+
+		name := fieldType.Name
+
+		// Skip unexported fields
+		if !unicode.IsUpper([]rune(name)[0]) {
+			continue
+		}
+
+		if tagVal, ok := fieldType.Tag.Lookup("section"); ok {
+			parts := strings.Split(tagVal, ",")
+			if parts[0] != "" {
+				name = parts[0]
+			}
+
+			if name == "-" {
+				continue
+			}
+		}
+
+		if err := encodeSection(fieldValue, name, result); err != nil {
+			return fmt.Errorf("failed to encode %s: %w", name, err)
+		}
+
+	}
+	return nil
+}
+
+func encodeSection(val reflect.Value, name string, result *File) error {
+	kind := getKind(val)
+	if kind == reflect.Ptr {
+		return encodeSection(reflect.Indirect(val), name, result)
+	}
+
+	if kind == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			if err := encodeSection(val.Index(i), name, result); err != nil {
+				return fmt.Errorf("failed to encode section at index %d: %w", i, err)
+			}
+		}
+
+		return nil
+	}
+
+	if kind != reflect.Struct {
+		return fmt.Errorf("cannot decode section from %s, expected a struct", kind)
+	}
+
+	var opts Options
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldValue := val.Field(i)
+		fieldType := val.Type().Field(i)
+
+		name := fieldType.Name
+
+		// skip unexported fields
+		if !unicode.IsUpper([]rune(name)[0]) {
+			continue
+		}
+
+		if tagValue, ok := fieldType.Tag.Lookup("option"); ok {
+			parts := strings.Split(tagValue, ",")
+			if parts[0] != "" {
+				name = parts[0]
+			}
+
+			if name == "-" {
+				continue
+			}
+		}
+
+		if err := encodeBasic(fieldValue, name, &opts); err != nil {
+			return fmt.Errorf("cannot encode value of option %s: %w", name, err)
+		}
+	}
+
+	result.Sections = append(result.Sections, Section{
+		Name:    name,
+		Options: opts,
+	})
+
+	return nil
+}
+
+func encodeBasic(val reflect.Value, name string, result *Options) error {
+	kind := getKind(val)
+	if kind == reflect.Ptr {
+		return encodeBasic(reflect.Indirect(val), name, result)
+	}
+
+	var value string
+	x := val.Interface()
+
+	switch kind {
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			elem := val.Index(i)
+			if err := encodeBasic(elem, name, result); err != nil {
+				return fmt.Errorf("failed to encode slice: %w", err)
+			}
+		}
+		return nil
+
+	case reflect.Bool:
+		value = strconv.FormatBool(x.(bool))
+	case reflect.Int, reflect.Uint:
+		value = fmt.Sprintf("%d", x)
+	case reflect.Float32:
+		value = strings.TrimRight(fmt.Sprintf("%f", x), "0")
+	case reflect.String:
+		value = x.(string)
+	default:
+		return fmt.Errorf("unsupported basic type %s", kind)
+	}
+
+	*result = append(*result, Option{
+		Name:  name,
+		Value: value,
+	})
+
+	return nil
+}
---
+++ conf/encode_utils_test.go
@@ -0,0 +1,80 @@
+package conf_test
+
+import (
+	"testing"
+
+	"github.com/ppacher/system-conf/conf"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodeSections(t *testing.T) {
+	type Sec1 struct {
+		Bool   bool
+		Int    int
+		Float  float32
+		String string
+	}
+
+	type Sec_2 struct {
+		Bool2   bool    `option:"Bool"`
+		Int2    int     `option:"Int"`
+		Float2  float64 `option:"Float"`
+		String2 string  `option:"String"`
+	}
+
+	type Sec_3 struct {
+		StringSlice []string
+	}
+
+	s := struct {
+		Sec1
+		Sec_2 Sec_2 `section:"Sec2"`
+		Sec_3 `section:"Sec3"`
+	}{
+		Sec1: Sec1{
+			Bool:   false,
+			Int:    10,
+			Float:  0.1,
+			String: "test",
+		},
+		Sec_2: Sec_2{
+			Bool2:   true,
+			Int2:    11,
+			Float2:  0.2,
+			String2: "test2",
+		},
+		Sec_3: Sec_3{
+			StringSlice: []string{"foo", "bar"},
+		},
+	}
+
+	file, err := conf.ConvertToFile(s, "")
+	assert.NoError(t, err)
+	assert.Equal(t, conf.Sections{
+		{
+			Name: "Sec1",
+			Options: conf.Options{
+				{Name: "Bool", Value: "false"},
+				{Name: "Int", Value: "10"},
+				{Name: "Float", Value: "0.1"},
+				{Name: "String", Value: "test"},
+			},
+		},
+		{
+			Name: "Sec2",
+			Options: conf.Options{
+				{Name: "Bool", Value: "true"},
+				{Name: "Int", Value: "11"},
+				{Name: "Float", Value: "0.2"},
+				{Name: "String", Value: "test2"},
+			},
+		},
+		{
+			Name: "Sec3",
+			Options: conf.Options{
+				{Name: "StringSlice", Value: "foo"},
+				{Name: "StringSlice", Value: "bar"},
+			},
+		},
+	}, file.Sections)
+}
